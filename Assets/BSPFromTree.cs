271b1df3bb33bac4a504ea27889ee44813cfeb28
svc-student <svc-student@student.directory.jccc>
2018-04-16 17:34:34 -0500

    Red Room of F

---
+++ Assets/BSPFromTree.cs
@@ -0,0 +1,53 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BSPFromTree : MonoBehaviour {
+
+    int height = 8;
+    int width = 8;
+    string[,] level;
+    Room test = new Room();
+    System.Text.StringBuilder sb = new System.Text.StringBuilder();
+
+    // Use this for initialization
+    void Start () {
+        level = new string[height, width];
+        test.left = 3;
+        test.top = 2;
+        test.width = 4;
+        test.height = 3;
+
+        for(int i = test.top; i < test.top + test.height; ++i)
+        {
+            for(int j = test.left; j < test.left + test.width; ++j)
+            {
+                level[i, j] = "F";
+            }
+        }
+
+        for (int i = 0; i < height; i++)
+        {
+            for (int j = 0; j < width; j++)
+            {
+                if (level[i, j] != "F")
+                    level[i, j] = "E";
+            }
+        }
+
+        for (int i = 0; i < height; i++)
+        {
+            for (int j = 0; j < width; j++)
+            {
+                sb.Append(level[i, j] + " ");
+            }
+            sb.AppendLine();
+        }
+        print(sb.ToString());
+    }
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
--- Assets/TreeRectDebug.cs
+++ Assets/TreeRectDebug.cs
@@ -10,24 +10,45 @@ public class TreeRectDebug : MonoBehaviour {
 
 	// Use this for initialization
 	void Start () {
-        BinaryTree<RectInt> sampleRectTree = new BinaryTree<RectInt>(new RectInt(0, 0, 20, 20));
+        BinaryTree<RectInt> sampleRectTree = new BinaryTree<RectInt>(new RectInt(0, 0, levelWidth, levelHeight));
 
         int partitionWidth = levelWidth / 2;
-        int parttionHeight = levelHeight;
+        int partitionHeight = levelHeight;
 
-        RectInt leftPartitionRect = new RectInt(0, 0, partitionWidth, parttionHeight);
+        RectInt leftPartitionRect = new RectInt(0, 0, partitionWidth, partitionHeight);
         BinaryTreeNode<RectInt> leftPartitionNode = sampleRectTree.Root().AddChild(leftPartitionRect);
 
         int rightPartitionX = levelWidth / 2;
         int rightPartitionY = 0;
-        RectInt rightPartitionRect = new RectInt(rightPartitionX, rightPartitionY, partitionWidth, parttionHeight);
+        RectInt rightPartitionRect = new RectInt(rightPartitionX, rightPartitionY, partitionWidth, partitionHeight);
         BinaryTreeNode<RectInt> rightPartitionNode = sampleRectTree.Root().AddChild(rightPartitionRect);
 
         RectInt leftPartitionWorld = NodeRectWorld(leftPartitionNode);
         RectInt rightPartitionWorld = NodeRectWorld(rightPartitionNode);
 
+        partitionHeight /= 2;
+        RectInt upperLeftPartitionRect = new RectInt(0, 0, partitionWidth, partitionHeight);
+        RectInt lowerLeftPartitionRect = new RectInt(0, partitionHeight, partitionWidth, partitionHeight);
+
+        RectInt upperRightPartitionRect = new RectInt(partitionWidth, 0, partitionWidth, partitionHeight);
+        RectInt lowerRightPartitionRect = new RectInt(partitionWidth, partitionHeight, partitionWidth, partitionHeight);
+
+        BinaryTreeNode<RectInt> uLPartitionNode = leftPartitionNode.AddChild(upperLeftPartitionRect);
+        BinaryTreeNode<RectInt> lLPartitionNode = leftPartitionNode.AddChild(lowerLeftPartitionRect);
+        BinaryTreeNode<RectInt> uRPartitionNode = rightPartitionNode.AddChild(upperRightPartitionRect);
+        BinaryTreeNode<RectInt> lRPartitionNode = rightPartitionNode.AddChild(lowerRightPartitionRect);
+
+        RectInt upperLeftNode = NodeRectWorld(uLPartitionNode);
+        RectInt lowerLeftNode = NodeRectWorld(lLPartitionNode);
+        RectInt upperRightNode = NodeRectWorld(uRPartitionNode);
+        RectInt lowerRightNode = NodeRectWorld(lRPartitionNode);
+
         print("Left: " + leftPartitionWorld);
         print("Right: " + rightPartitionWorld);
+        print("upper left: " + upperLeftPartitionRect);
+        print("lower left: " + lowerLeftPartitionRect);
+        print("upper right: " + upperRightPartitionRect);
+        print("lower right: " + lowerRightPartitionRect);
     }
 
     private RectInt NodeRectWorld(BinaryTreeNode<RectInt> node)

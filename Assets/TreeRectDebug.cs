e9a8999f559ab0c3105f033b524dfa86e4d9671f
svc-student <svc-student@student.directory.jccc>
2018-04-18 17:33:15 -0500

    Quad Partitions Recognized

--- Assets/TreeRectDebug.cs
+++ Assets/TreeRectDebug.cs
@@ -7,9 +7,15 @@ public class TreeRectDebug : MonoBehaviour {
 
     public int levelWidth = 20;
     public int levelHeight = 20;
+    System.Text.StringBuilder sb = new System.Text.StringBuilder();
+
+    private string[,] level;
 
 	// Use this for initialization
 	void Start () {
+
+        level = new string[levelHeight, levelWidth];
+
         BinaryTree<RectInt> sampleRectTree = new BinaryTree<RectInt>(new RectInt(0, 0, levelWidth, levelHeight));
 
         int partitionWidth = levelWidth / 2;
@@ -30,8 +36,8 @@ public class TreeRectDebug : MonoBehaviour {
         RectInt upperLeftPartitionRect = new RectInt(0, 0, partitionWidth, partitionHeight);
         RectInt lowerLeftPartitionRect = new RectInt(0, partitionHeight, partitionWidth, partitionHeight);
 
-        RectInt upperRightPartitionRect = new RectInt(partitionWidth, 0, partitionWidth, partitionHeight);
-        RectInt lowerRightPartitionRect = new RectInt(partitionWidth, partitionHeight, partitionWidth, partitionHeight);
+        RectInt upperRightPartitionRect = new RectInt(0, 0, partitionWidth, partitionHeight);
+        RectInt lowerRightPartitionRect = new RectInt(0, partitionHeight, partitionWidth, partitionHeight);
 
         BinaryTreeNode<RectInt> uLPartitionNode = leftPartitionNode.AddChild(upperLeftPartitionRect);
         BinaryTreeNode<RectInt> lLPartitionNode = leftPartitionNode.AddChild(lowerLeftPartitionRect);
@@ -45,10 +51,34 @@ public class TreeRectDebug : MonoBehaviour {
 
         print("Left: " + leftPartitionWorld);
         print("Right: " + rightPartitionWorld);
+
         print("upper left: " + upperLeftPartitionRect);
         print("lower left: " + lowerLeftPartitionRect);
         print("upper right: " + upperRightPartitionRect);
         print("lower right: " + lowerRightPartitionRect);
+
+        print("upper left: " + upperLeftNode);
+        print("lower left: " + lowerLeftNode);
+        print("upper right: " + upperRightNode);
+        print("lower right: " + lowerRightNode);
+
+        MakeRoom(upperLeftNode, "1");
+        MakeRoom(lowerLeftNode, "2");
+        MakeRoom(upperRightNode, "3");
+        MakeRoom(lowerRightNode, "4");
+
+        for (int i = 0; i < levelHeight; i++)
+        {
+            for (int j = 0; j < levelWidth; j++)
+            {
+                sb.Append(level[i, j] + " ");
+            }
+            sb.AppendLine();
+        }
+        print(sb.ToString());
+
+        
+        
     }
 
     private RectInt NodeRectWorld(BinaryTreeNode<RectInt> node)
@@ -64,6 +94,20 @@ public class TreeRectDebug : MonoBehaviour {
             current = current.parent;
         }
         return rectWorld;
+    }
+
+    private void MakeRoom(RectInt region, string value)
+    {
+        // TODO: Set the values of level[,] that are inside
+        // the area specified by RectInt region
+
+        for(int i = region.y; i < region.y + region.height; ++i)
+        {
+            for(int j = region.x; j < region.x + region.width; ++j)
+            {
+                level[i, j] = value;
+            }
+        }
     }
 	
 	// Update is called once per frame
